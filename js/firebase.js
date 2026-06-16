8bb2676380010983ee3efa6524d0e75f106e3832
tkhr4940-svg <tkhr4940@gmail.com>
2026-06-16 22:32:19 +0900

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,10 +3,10 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "あなたのapiKey",
+  apiKey: "AIzaSyAhIC6JEKhT1dzxdWyBvK4JPNsWm2_bm1c",
   authDomain: "sports-event-live.firebaseapp.com",
   projectId: "sports-event-live",
-  storageBucket: "あなたのstorageBucket",
+  storageBucket: "sports-event-live.firebasestorage.app",
   messagingSenderId: "966316263895",
   appId: "1:966316263895:web:ad98e427e08ab00f23a392"
 };
