c6464d27a7acd2610db995e7c2ee49105168c92d
tkhr4940-svg <tkhr4940@gmail.com>
2026-06-16 14:04:19 +0900

    firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "あなたのapiKey",
+  authDomain: "あなたのauthDomain",
+  projectId: "あなたのprojectId",
+  storageBucket: "あなたのstorageBucket",
+  messagingSenderId: "あなたのmessagingSenderId",
+  appId: "あなたのappId"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
